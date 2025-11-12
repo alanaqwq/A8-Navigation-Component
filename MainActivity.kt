65be4b59a73fa9099de5867fe424db99f80524a0
alanaqwq <143505994+alanaqwq@users.noreply.github.com>
2025-11-12 13:06:59 -0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,11 @@
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}
